ba9e9f11220124c960441e0b2caa463428c4e5e0
illheney <84401757+illheney@users.noreply.github.com>
2026-09-13 14:41:25 -0400

    Refresh offline cache for Held v1.1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="held-v1";
-const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg"];
+const CACHE="held-v1.1";
+const ASSETS=["./","./index.html","./styles.css","./polish.css","./app.js","./polish.js","./manifest.webmanifest","./icons/apple-touch-icon.png","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
 self.addEventListener("fetch",e=>{
